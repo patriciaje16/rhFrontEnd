2839e8026206cf6c938fa8b90cb6fe6d6d8cd37f
Patricia Edwards <patriciaje16@gmail.com>
2022-06-09 17:10:50 +0100

    list styling

--- src/components/HousewivesList/HousewivesList.jsx
+++ src/components/HousewivesList/HousewivesList.jsx
@@ -5,12 +5,12 @@ import Housewife from "../Housewife/Housewife"
 
 const HousewivesList = (props) => {
   return (
-    <>
-      <h2 className="heading">Past and Current Real Housewives</h2>
+    <div className="list">
+      <h2 className="housewives__header">Past and Current Real Housewives</h2>
       <div className="container">
       {props.housewives && props.housewives.map(housewife => <Housewife key={housewife.id} housewife={housewife} />)}
       </div>
-    </>
+    </div>
   )
 }
 

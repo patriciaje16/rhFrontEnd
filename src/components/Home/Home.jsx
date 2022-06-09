d21863457e13ca89ddfcf4d0f6802314fc25fcc9
Patricia Edwards <patriciaje16@gmail.com>
2022-06-09 17:10:35 +0100

    home page styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import "./Home.scss";
 
+
 import React from 'react'
+
 
 const Home = () => {
   return (
-    <h1>Welcome to the Real Housewives Catalogue</h1>
+    <div class="background">
+    
+    
+    <h1 className="heading">Welcome to the Real Housewives Catalogue</h1>
+    
+    </div>
   )
 }
 
